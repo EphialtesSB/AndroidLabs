a99990c9fc33817fdc28690255d22e4ad5cd7416
Serge <Serge_barrette@hotmail.com>
2020-02-13 17:06:31 -0500

    EphialtedSB 1st commit

---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mimageButton.setImageBitmap(imageBitmap);
+        }
+    }
+}
